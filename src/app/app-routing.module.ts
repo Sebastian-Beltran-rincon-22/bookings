5bb0acb7534292b3832d694893ad0f1483eca977
sewastian <sebastianrincon834@gmail.com>
2024-03-05 16:02:59 -0500

    feat [app -> app.routing.module.ts]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { BookingsComponent } from './pages/bookings/bookings.component';
+import { PagesNotFoundComponent } from './errors/pages-not-found/pages-not-found.component';
+import { guardGuard } from 'src/guard/guard.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home/:id', component:HomeComponent, canActivate:[guardGuard]},
+  {path:'', component: LoginComponent},
+  {path:'bookings', component:BookingsComponent, canActivate:[guardGuard]},
+
+
+  //404
+  {path:'**',component:PagesNotFoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
