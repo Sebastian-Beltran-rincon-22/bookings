aa093ed1d78e82544c7f6970c927ab01a886ed33
sewastian <sebastianrincon834@gmail.com>
2024-03-05 16:28:25 -0500

    feat [guard -> guard.guard.ts]

---
+++ src/guard/guard.guard.ts
@@ -0,0 +1,15 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { Inject,inject } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
+
+export const guardGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService)
+  const router = inject(Router)
+
+  if(authService.loggedIn()){
+    return true
+  }else{
+    router.navigate(['**'])
+    return false
+  }
+};
